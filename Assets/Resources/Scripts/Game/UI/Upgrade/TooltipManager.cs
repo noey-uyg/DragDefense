9a0195061ad4335fb8d540256a653690b3a7d991
Noeyuyg <dnlrbdus31@gmail.com>
2026-01-18 21:26:27 +0900

    - modify : 툴팁 위치 오류 수정

--- Assets/Resources/Scripts/Game/UI/Upgrade/TooltipManager.cs
+++ Assets/Resources/Scripts/Game/UI/Upgrade/TooltipManager.cs
@@ -6,9 +6,25 @@ public class TooltipManager : Singleton<TooltipManager>
     [SerializeField] private RectTransform _canvasRect;
     [SerializeField] private Camera _UICamera;
 
+    private Vector2 _canvasRectSize;
+
+    protected override void OnAwake()
+    {
+        base.OnAwake();
+        _canvasRectSize = _canvasRect.rect.size;
+    }
+
     public void ShowUpgradeTooltip(string title, string desc, string cost, int curLevel, int maxLevel, Vector2 position)
     {
-        _upgradeTooltip.Show(title, desc, cost, curLevel, maxLevel, position);
+        Vector2 screenPos = RectTransformUtility.WorldToScreenPoint(_UICamera, position);
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            _canvasRect,
+            screenPos,
+            _UICamera,
+            out Vector2 localPos);
+
+        _upgradeTooltip.Show(title, desc, cost, curLevel, maxLevel, localPos, _canvasRectSize);
     }
 
     public void HideTooltip()
--- Assets/Resources/Scripts/Game/UI/Upgrade/UpgradeTooltip.cs
+++ Assets/Resources/Scripts/Game/UI/Upgrade/UpgradeTooltip.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpgradeTooltip : MonoBehaviour
 {
@@ -8,8 +9,9 @@ public class UpgradeTooltip : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _costText;
     [SerializeField] private TextMeshProUGUI _levelText;
     [SerializeField] private RectTransform _rectTransform;
+    [SerializeField] private float _offsetY = 60f;
 
-    public void Show(string title, string desc, string cost, int curLevel, int maxLevel, Vector2 position)
+    public void Show(string title, string desc, string cost, int curLevel, int maxLevel, Vector2 position, Vector2 canvasSize)
     {
         gameObject.SetActive(true);
         _titleText.text = title;
@@ -17,11 +19,37 @@ public class UpgradeTooltip : MonoBehaviour
         _costText.text = $"Cost : {cost}";
         _levelText.text = $"Lv. {curLevel} / {maxLevel}";
 
-        _rectTransform.anchoredPosition = position + new Vector2(0, 60);
+        _rectTransform.anchoredPosition = position + new Vector2(0, _offsetY);
+
+        LayoutRebuilder.ForceRebuildLayoutImmediate(_rectTransform);
+
+        ClampToScreen(position, canvasSize);
     }
 
     public void Hide()
     {
         gameObject.SetActive(false);
+    }
+
+    private void ClampToScreen(Vector2 nodeLoaclPos, Vector2 canvasSize)
+    {
+        Vector2 pos = _rectTransform.anchoredPosition;
+        Vector2 size = _rectTransform.rect.size;
+        Vector2 pivot = _rectTransform.pivot;
+
+        float maxY = pos.y + (size.y * (1f - pivot.y));
+
+        if(maxY > canvasSize.y / 2) pos.y = nodeLoaclPos.y - _offsetY - size.y;
+
+        float minX = pos.x - (size.x * pivot.x);
+        float maxX = pos.x + (size.x * (1f - pivot.x));
+
+        if (minX < -canvasSize.x / 2) pos.x += (-canvasSize.x / 2 - minX);
+        if (maxX > canvasSize.x / 2) pos.x -= (maxX - canvasSize.x / 2);
+
+        float minY = pos.y - (size.y * pivot.y);
+        if (minY < -canvasSize.y / 2) pos.y += (-canvasSize.y / 2 - minY);
+
+        _rectTransform.anchoredPosition = pos;
     }
 }
